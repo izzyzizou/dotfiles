d0c5385a2e84d0a8830b44fc957e7f14a14e7fbd
izzat <izzyzizou@ymail.com>
2026-09-17 15:50:46 -0400

    update Brewfile and zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,8 @@ alias py="python3"
 alias c="code ."
 alias v="nvim"
 alias b="~/.dotfiles/script/bootstrap"
+alias s="spotify_player"
+alias h="herdr"
+sync-fork() {
+    gh repo sync "izzyzizou/$1"
+}
